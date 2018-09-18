5bb76ad6865770372e22f2e30f77eedb30f9d5c2
djamaile <rdjamaile@gmail.com>
2018-09-18 11:32:56 +0200

    Datum converted

--- src/components/AppHeader.js
+++ src/components/AppHeader.js
@@ -3,7 +3,7 @@ import {BrowserRouter, Route, Switch, Link, NavLink} from 'react-router-dom';
 
 const Header = () => (
     <header>
-        <h1>Aanvraag Applicatie</h1>
+        <h1>React Applicatie</h1>
         <NavLink to="/" activeClassName="isActive" exact={true}>Dashboard</NavLink>
         <NavLink to="/aanvraag" activeClassName="isActive">Aanvraag</NavLink>
     </header>
--- src/components/aankoopLijstItem.js
+++ src/components/aankoopLijstItem.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
-import {verwijderAankoop, editAankoop} from '../actions/aankopen';
 import {connect} from 'react-redux';
+import moment from 'moment';
 
 const AankoopLijstItem = ({dispatch,description, totaal, aangemaakt, id, update}) => (
     <div>
         <Link to={`/edit/${id}`}>
             <h4>Description: {description}</h4>
         </Link>
-        <p>Prijs: {totaal}</p>
-        <p>Aangemaakt: {aangemaakt}</p>
+        <p>Bedrag: €{totaal}</p>
+        <p>Aangemaakt: {moment(aangemaakt).format('Do MMMM, YYYY')}</p>
     </div>
 );
 
